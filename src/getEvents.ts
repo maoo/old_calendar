fbfc08c66615ca6e5dfe0e556dece9d213c7c20c
Juan Estrella <juan.estrella@finos.org>
2023-07-27 10:33:41 +0200

    add .env

--- src/getEvents.ts
+++ src/getEvents.ts
@@ -1,9 +1,14 @@
 import { calendar_v3, google } from 'googleapis';
 import fs from 'fs';
 
-// Replace with the path to your service account JSON file
-const SERVICE_ACCOUNT_FILE = './calendar-service-account.json';
+const dotenv = require('dotenv');
+dotenv.config();
 
+const PRIVATE_KEY_ID = process.env.PRIVATE_KEY_ID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// Replace with the path to your service account JSON file
+const SERVICE_ACCOUNT_FILE = `{ "type": "service_account", "project_id": "calendar-393913", "private_key_id": ${PRIVATE_KEY_ID}, "private_key": ${PRIVATE_KEY}, "client_email": "finos-calendar@calendar-393913.iam.gserviceaccount.com", "client_id": "104234361585971910357", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/finos-calendar%40calendar-393913.iam.gserviceaccount.com", "universe_domain": "googleapis.com"}`;
 // Scopes required for the Google Calendar API
 const SCOPES = ['https://www.googleapis.com/auth/calendar',
 'https://www.googleapis.com/auth/calendar.events',
